d6300001c13a1ec13fa670d47c37a2e842d37ba5
Outvi V <git@outv.im>
2020-04-12 14:51:32 +0800

    feat: move subscriptionRequest to utils

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+const fetch = require("node-fetch");
+
 function getServerPort(config) {
   if (config.SERVER_PORT) return config.SERVER_PORT;
   if (config.CALLBACK_URL) {
@@ -17,14 +19,41 @@ function getSubpath(config) {
     try {
       let url = new URL(config.CALLBACK_URL);
       return url.pathname ? url.pathname : "/";
-    // eslint-disable-next-line no-empty
+      // eslint-disable-next-line no-empty
     } finally {
     }
   }
   return "/";
 }
 
+function subscriptionRequest(
+  callback,
+  topic,
+  address = "https://pubsubhubbub.appspot.com/subscribe"
+) {
+  let urlencoded = new URLSearchParams();
+  urlencoded.append("hub.callback", callback);
+  urlencoded.append("hub.verify", "async");
+  urlencoded.append("hub.mode", "subscribe");
+  urlencoded.append("hub.topic", topic);
+
+  fetch(address, {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: urlencoded,
+    method: "POST",
+  })
+    .then(() => {
+      console.log(`Subscription request sent for ${topic}`);
+    })
+    .catch((err) => {
+      console.log(`Subscription request failed for ${topic}: ${err}`);
+    });
+}
+
 module.exports = {
   getServerPort,
   getSubpath,
+  subscriptionRequest,
 };
